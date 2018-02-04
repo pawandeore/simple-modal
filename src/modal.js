1bc7df6e0b5d21c1b65af7c5b17d84ecd247d46e
Bryce Jech <bjech@osugiving.com>
2018-02-03 22:05:54 -0600

    switch to event delegation, add index file

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,10 @@
 'use strict';
 
+
+
 (function(){
+
+    var delay = 250;
 
     // Modal toggler
     function modal(el){
@@ -12,66 +16,66 @@
             return;
         }
         if(el.classList.contains('active')){
-            // Hide
-            el.classList.remove('active');
-            setTimeout(function(){
-                el.style.display = 'none';
-                document.body.classList.remove('modal-active');
-            }, 250);
+            _hideModal(el);
         }
         else{
-            // Show
-            el.style.display = 'block';
-            setTimeout(function(){
-                el.classList.add('active');
-                document.body.classList.add('modal-active');
-            }, 25);
+            _showModal(el);
         }
     }
 
-    // Add event listeners to modal launching elems
-    Array.prototype.slice.call(document.querySelectorAll('[data-toggle="modal"][data-target]'))
-        .forEach(function(elem){
-            var target = document.querySelector(elem.getAttribute('data-target'));
+    function _showModal(el){
+        el.style.display = 'block';
 
-            elem.addEventListener('click', function(event){
-                modal(target);
+        setTimeout(function(){
+            el.classList.add('active');
+            document.body.classList.add('modal-active');
+        }, delay);
+
+    }
+
+    function _hideModal(el){
+        el.classList.remove('active');
+
+        setTimeout(function(){
+            el.style.display = 'none';
+            document.body.classList.remove('modal-active');
+        }, delay);
+    }
+
+    function _hideAll(){
+        Array.prototype.slice.call(document.querySelectorAll('.modal.active'))
+            .forEach(function(el){
+                _hideModal(el);
             });
-        });
-
-    // Clicking on modal background should close modal
-    Array.prototype.slice.call(document.querySelectorAll('.modal'))
-        .forEach(function(elem){
-            elem.addEventListener('click', function(event){
-                if(event.target == elem){
-                    elem.classList.remove('active');
-                    setTimeout(function(){
-                        elem.style.display = 'none';
-                        document.body.classList.remove('modal-active');
-                    }, 250);
-                }
-            })
-        });
+    }
 
     // Exit key should close any open modals
     document.addEventListener('keyup', function(event){
         if(event.keyCode === 27){
-            Array.prototype.slice.call(document.querySelectorAll('.modal.active'))
-                .forEach(function(elem){
-                    elem.classList.remove('active');
-                    setTimeout(function(){
-                        elem.style.display = 'none';
-                        document.body.classList.remove('modal-active');
-                    }, 250);
-                });
+            _hideAll();
         }
     });
+
+    document.addEventListener('click', function(e){
+        var el = e.target;
+
+        if(el.getAttribute('data-toggle') == 'modal'){
+            var modal = document.querySelector(el.getAttribute('data-target'));
+            _showModal(modal);
+        }
+        else if(el.classList.contains('modal')){
+            _hideModal(el);
+        }
+        else if(el.classList.contains('modal-close')){
+            _hideAll();
+        }
+    });
+
 
     // Expose modal functionality
     // Named elements ([id], [name]) appear as properties on window object
     // This ensures that we are only overriding window.modal if window.modal is a DOM elem
     //https://stackoverflow.com/a/3434388
-    // if(typeof window.modal !== 'function') window.modal = modal;
-    // if(window.modal instanceof Element) window.modal = modal;
-    window.modal = (window.modal === document.getElementById('modal')) ? modal : window.modal
+    if(typeof window.modal !== 'function') window.modal = modal;
+
 })();
